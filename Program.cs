2872ed0ab5bd4e8247b422488934f7ca89679152
Hp <Hp@DESKTOP-3N3NI39>
2024-11-02 14:18:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using BarcodeLib;
+using System;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите текст для кодирования:");
+            string inputText = Console.ReadLine();
+
+            // Создание объекта Barcode
+            Barcode barcode = new Barcode(inputText);
+
+            // Вывод штрих-кода
+            Console.WriteLine(barcode.ToString());
+            Console.ReadKey();
+        }
+    }
+}
